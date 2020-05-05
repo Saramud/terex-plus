647fc7faebedf6e97e73fe7345ce762e32dfa553
Sergey Saramud <s.saramud@mashtab.org>
2020-05-05 23:05:40 +0300

    Дело аккордеона движется к концу

--- src/js/lib.js
+++ src/js/lib.js
@@ -46,8 +46,17 @@ $("#sendMail").on("click", function(){
     
 });
 
+//аккордеон
+$('.panel-collapse [data-toggle="collapse"]').on('click', function() {
+	var $target = $(this).parents('.panel-collapse');
+	$target.removeClass('in').addClass('show');
+	setTimeout(function() {
+	  $target.removeClass('show').addClass('in');
+	}, 10)
+  });
+
 //Перенос на участок с отправкой заявки на главной странице
-    $('a[href^="#"], *[data-href^="#"]').on('click', function(e){
+    $('.indexPage').on('click', function(e){
         e.preventDefault();
         let t = 1000;
         let d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
