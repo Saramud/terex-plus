f7d68ec71a262001f0b69aeab42384898df215ea
s.saramud <s.saramud@mashtab.org>
2020-11-02 00:19:27 +0300

    Добавлены отдельные страницы для каждого вида техники. Ух и нагородил я лес

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,7 +1,6 @@
 
-
 /*Переменные*/
-let pos;
+
 let propertiesCommon = { 
 
     beadLength: 'Длина борта, м',
@@ -28,53 +27,53 @@ let propertiesCommon = {
 };
 //!!!!! ВНИМАТЕЛЬНО, БАЗА ТЕХНИКИ ЕЩЕ ЕСТЬ В techMenu.pug, при зименении нужно сверять
 let name = {
-    atkrnKAMAZVEZDEHOD: ['Автокран КАМАЗ "Галичанин" 25 тонн', '/build/img/atkrnKAMAZVEZDEHOD.webp', '/technics/atkrnKAMAZVEZDEHOD.html'],
-    atkrnMaz: ['Автокран МАЗ 14 тонн', '/build/img/atkrnMaz.webp', '/technics/atkrnMaz.html'],
-    atkrURAL55733: ['Автокран Урал КС-55733 ЧЕЛЯБИНЕЦ 32 тонны', '/build/img/atkrURAL55733.webp', '/technics/atkrURAL55733.html'],
-    atkrKAMAZ65115: ['АВТОКРАН КАМАЗ-65115 "КЛИНЦЫ" КС-55713-1К-3 25 ТОНН', '/build/img/atkrKAMAZ65115.webp', '/technics/atkrKAMAZ65115.html'],
-    atkrKAMAZ63501: ['АВТОКРАН КАМАЗ-63501 «КЛИНЦЫ» КС-65719-1К 40 ТОНН', '/build/img/atkrKAMAZ63501.webp', '/technics/atkrKAMAZ63501.html'],
+    atkrnKAMAZVEZDEHOD: ['Автокран КАМАЗ "Галичанин" 25 тонн', '/build/img/atkrnKAMAZVEZDEHOD.webp', '/technics/autokrans/atkrnKAMAZVEZDEHOD.html'],
+    atkrnMaz: ['Автокран МАЗ 14 тонн', '/build/img/atkrnMaz.webp', '/technics/autokrans/atkrnMaz.html'],
+    atkrURAL55733: ['Автокран Урал КС-55733 ЧЕЛЯБИНЕЦ 32 тонны', '/build/img/atkrURAL55733.webp', '/technics/autokrans/atkrURAL55733.html'],
+    atkrKAMAZ65115: ['АВТОКРАН КАМАЗ-65115 "КЛИНЦЫ" КС-55713-1К-3 25 ТОНН', '/build/img/atkrKAMAZ65115.webp', '/technics/autokrans/atkrKAMAZ65115.html'],
+    atkrKAMAZ63501: ['АВТОКРАН КАМАЗ-63501 «КЛИНЦЫ» КС-65719-1К 40 ТОНН', '/build/img/atkrKAMAZ63501.webp', '/technics/autokrans/atkrKAMAZ63501.html'],
     
-    mineKubota: ['Мини-экскаватор Kubota KX057-4', '/build/img/mineKubota.webp', '/technics/mineKubota.html'],
-    mineHitachiEX30: ['Мини-экскаватор HITACHI EX30 UR2', '/build/img/mineHitachiEX30.webp', '/technics/mineHitachiEX30.html'],
-    mineTakeuchiTB016: ['Мини-экскаватор TAKEUCHI TB016', '/build/img/mineTakeuchiTB016.webp', '/technics/mineTakeuchiTB016.html'],
+    mineKubota: ['Мини-экскаватор Kubota KX057-4', '/build/img/mineKubota.webp', '/technics/mini-excavatory/mineKubota.html'],
+    mineHitachiEX30: ['Мини-экскаватор HITACHI EX30 UR2', '/build/img/mineHitachiEX30.webp', '/technics/mini-excavatory/mineHitachiEX30.html'],
+    mineTakeuchiTB016: ['Мини-экскаватор TAKEUCHI TB016', '/build/img/mineTakeuchiTB016.webp', '/technics/mini-excavatory/mineTakeuchiTB016.html'],
     
-    smsvlKamaz6520: ['Самосвал КАМАЗ 6520', '/build/img/smsvlKamaz6520.webp', '/technics/smsvlKamaz6520.html'],
-    smsvlKamaz65115: ['Самосвал КАМАЗ 65115', '/build/img/smsvlKamaz65115.webp', '/technics/smsvlKamaz65115.html'],
-    smsvlReanaultKerax: ['Самосвал RENAULT KERAX 8Х4', '/build/img/smsvlReanaultKerax.webp', '/technics/smsvlReanaultKerax.html'],
+    smsvlKamaz6520: ['Самосвал КАМАЗ 6520', '/build/img/smsvlKamaz6520.webp', '/technics/samosavls/smsvlKamaz6520.html'],
+    smsvlKamaz65115: ['Самосвал КАМАЗ 65115', '/build/img/smsvlKamaz65115.webp', '/technics/samosavls/smsvlKamaz65115.html'],
+    smsvlReanaultKerax: ['Самосвал RENAULT KERAX 8Х4', '/build/img/smsvlReanaultKerax.webp', '/technics/samosavls/smsvlReanaultKerax.html'],
     
-    egusHitachi330: ['Экскаватор гусеничный HITACHI 330', '/build/img/egusHitachi330.webp', '/technics/egusHitachi330.html'],
-    egusHitachi300: ['Экскаватор гусеничный HITACHI 300', '/build/img/egusHitachi300.webp', '/technics/egusHitachi300.html'],
-    egusVOLVOEC240: ['Экскаватор гусеничный VOLVO EC 240 PRAIM', '/build/img/egusVOLVOEC240.webp', '/technics/egusVOLVOEC240.html'],
-    egusVOLVOEC240LOng: ['Экскаватор гусеничный длиннорукий VOLVO EC 240', '/build/img/egusVOLVOEC240LOng.webp', '/technics/egusVOLVOEC240LOng.html'],
+    egusHitachi330: ['Экскаватор гусеничный HITACHI 330', '/build/img/egusHitachi330.webp', '/technics/excavatory-gusenichnye/egusHitachi330.html'],
+    egusHitachi300: ['Экскаватор гусеничный HITACHI 300', '/build/img/egusHitachi300.webp', '/technics/excavatory-gusenichnye/egusHitachi300.html'],
+    egusVOLVOEC240: ['Экскаватор гусеничный VOLVO EC 240 PRAIM', '/build/img/egusVOLVOEC240.webp', '/technics/excavatory-gusenichnye/egusVOLVOEC240.html'],
+    egusVOLVOEC240LOng: ['Экскаватор гусеничный длиннорукий VOLVO EC 240', '/build/img/egusVOLVOEC240LOng.webp', '/technics/excavatory-gusenichnye/egusVOLVOEC240LOng.html'],
     
-    ekolDoosan: ['Экскаватор колёсный DOOSAN SOLAR 180', '/build/img/ekolDoosan.webp', '/technics/ekolDoosan.html'],
-    ekolJCBJS200W: ['Экскаватор колёсный JCB JS200W', '/build/img/ekolJCBJS200W.webp', '/technics/ekolJCBJS200W.html'],
-    ekolVOLVOEW160: ['Экскаватор колёсный VOLVO EW 160', '/build/img/ekolVOLVOEW160.webp', '/technics/ekolVOLVOEW160.html'], 
+    ekolDoosan: ['Экскаватор колёсный DOOSAN SOLAR 180', '/build/img/ekolDoosan.webp', '/technics/excavatory-kolesnye/ekolDoosan.html'],
+    ekolJCBJS200W: ['Экскаватор колёсный JCB JS200W', '/build/img/ekolJCBJS200W.webp', '/technics/excavatory-kolesnye/ekolJCBJS200W.html'],
+    ekolVOLVOEW160: ['Экскаватор колёсный VOLVO EW 160', '/build/img/ekolVOLVOEW160.webp', '/technics/excavatory-kolesnye/ekolVOLVOEW160.html'], 
     
-    epogTLB825: ['Экскаватор погрузчик terex TLB 825', '/build/img/epogTLB825.webp', '/technics/epogTLB825.html'],
-    epogJSB4CX: ['Экскаватор погрузчик JCB 4CX', '/build/img/epogJSB4CX.webp', '/technics/epogJSB4CX.html'],
-    epogTEREX860: ['Экскаватор погрузчик TEREX 860' ,'/build/img/epogTEREX860.webp', '/technics/epogTEREX860.html'],
+    epogTLB825: ['Экскаватор погрузчик terex TLB 825', '/build/img/epogTLB825.webp', '/technics/excavatory-pogruzchiki/epogTLB825.html'],
+    epogJSB4CX: ['Экскаватор погрузчик JCB 4CX', '/build/img/epogJSB4CX.webp', '/technics/excavatory-pogruzchiki/epogJSB4CX.html'],
+    epogTEREX860: ['Экскаватор погрузчик TEREX 860' ,'/build/img/epogTEREX860.webp', '/technics/excavatory-pogruzchiki/epogTEREX860.html'],
     
-    teleskPogrBobcatT40170: ['Телескопический погрузчик BOBCAT T40170', '/build/img/teleskPogrBobcatT40170.webp', '/technics/teleskPogrBobcatT40170.html' ],
-    teleskPogrFaresinFH1442: ['Телескопический погрузчик FARESIN FH 14.42', '/build/img/teleskPogrFaresinFH1442.webp', '/technics/teleskPogrFaresinFH1442.html' ],
-    teleskPogrJCBAGRI: ['Телескопический погрузчик JCB AGRI 536-60', '/build/img/teleskPogrJCBAGRI.webp', '/technics/teleskPogrJCBAGRI.html' ],
+    teleskPogrBobcatT40170: ['Телескопический погрузчик BOBCAT T40170', '/build/img/teleskPogrBobcatT40170.webp', '/technics/teleskopicheskie-pogruzchiki/teleskPogrBobcatT40170.html' ],
+    teleskPogrFaresinFH1442: ['Телескопический погрузчик FARESIN FH 14.42', '/build/img/teleskPogrFaresinFH1442.webp', '/technics/teleskopicheskie-pogruzchiki/teleskPogrFaresinFH1442.html' ],
+    teleskPogrJCBAGRI: ['Телескопический погрузчик JCB AGRI 536-60', '/build/img/teleskPogrJCBAGRI.webp', '/technics/teleskopicheskie-pogruzchiki/teleskPogrJCBAGRI.html' ],
     
-    minPogruzWECAN800D:  ['Мини-погрузчик WECAN 800D', '/build/img/minPogruzWECAN800D.webp', '/technics/minPogruzWECAN800D.html' ],
-    minPogruzTAKEUCHI:  ['Мини-погрузчик TAKEUCHI TL140', '/build/img/minPogruzTAKEUCHI.webp', '/technics/minPogruzTAKEUCHI.html' ],
-    minPogruzNEWHOLLANDL218:  ['Мини-погрузчик NEW HOLLAND L 218', '/build/img/minPogruzNEWHOLLANDL218.webp', '/technics/minPogruzNEWHOLLANDL218.html' ],
+    minPogruzWECAN800D:  ['Мини-погрузчик WECAN 800D', '/build/img/minPogruzWECAN800D.webp', '/technics/mini-pogruzchiki/minPogruzWECAN800D.html' ],
+    minPogruzTAKEUCHI:  ['Мини-погрузчик TAKEUCHI TL140', '/build/img/minPogruzTAKEUCHI.webp', '/technics/mini-pogruzchiki/minPogruzTAKEUCHI.html' ],
+    minPogruzNEWHOLLANDL218:  ['Мини-погрузчик NEW HOLLAND L 218', '/build/img/minPogruzNEWHOLLANDL218.webp', '/technics/mini-pogruzchiki/minPogruzNEWHOLLANDL218.html' ],
     
-    frontPogruzVOLVOL90F: ['Фронтальный погрузчик VOLVO L90F', '/build/img/frontPogruzVOLVOL90F.webp', '/technics/frontPogruzVOLVOL90F.html' ],
-    frontPogruzLG936L: ['Фронтальный погрузчик LG936L', '/build/img/frontPogruzLG936L.webp', '/technics/frontPogruzLG936L.html' ],
-    frontPogruzLG933L: ['Фронтальный погрузчик LG933L', '/build/img/frontPogruzLG933L.webp', '/technics/frontPogruzLG933L.html' ],
+    frontPogruzVOLVOL90F: ['Фронтальный погрузчик VOLVO L90F', '/build/img/frontPogruzVOLVOL90F.webp', '/technics/frontalnye-pogruzchiki/frontPogruzVOLVOL90F.html' ],
+    frontPogruzLG936L: ['Фронтальный погрузчик LG936L', '/build/img/frontPogruzLG936L.webp', '/technics/frontalnye-pogruzchiki/frontPogruzLG936L.html' ],
+    frontPogruzLG933L: ['Фронтальный погрузчик LG933L', '/build/img/frontPogruzLG933L.webp', '/technics/frontalnye-pogruzchiki/frontPogruzLG933L.html' ],
    
-    mnpltrHD78: ['Манипулятор Hyundai HD 78', '/build/img/mnpltrHD78.webp', '/technics/mnpltrHD78.html'],
-    mnpltrHD120: ['Манипулятор Hyundai HD 120', '/build/img/mnpltrHD120.webp', '/technics/mnpltrHD120.html'],
-    mnpltrKamaz: ['Манипулятор КАМАЗ', '/build/img/mnpltrKamaz.webp', '/technics/mnpltrKamaz.html'],
+    mnpltrHD78: ['Манипулятор Hyundai HD 78', '/build/img/mnpltrHD78.webp', '/technics/manipulators/mnpltrHD78.html'],
+    mnpltrHD120: ['Манипулятор Hyundai HD 120', '/build/img/mnpltrHD120.webp', '/technics/manipulators/mnpltrHD120.html'],
+    mnpltrKamaz: ['Манипулятор КАМАЗ', '/build/img/mnpltrKamaz.webp', '/technics/manipulators/mnpltrKamaz.html'],
 
 };
 
 let all = {
-    autokran: { 
+    autokranM: { 
         img: [name.atkrnKAMAZVEZDEHOD[1],name.atkrnMaz[1], name.atkrURAL55733[1], name.atkrKAMAZ65115[1], name.atkrKAMAZ63501[1]],
         title: [name.atkrnKAMAZVEZDEHOD[0],name.atkrnMaz[0], name.atkrURAL55733[0], name.atkrKAMAZ65115[0], name.atkrKAMAZ63501[0]],
         description: ['amet consectetur adipisicing elit.', 'amet consectetur adipisicing elit.', 'amet consectetur adipisicing elit.'],
@@ -88,7 +87,7 @@ let all = {
         carryingСapacity:['25', '14', '32', '25', '40'],
         link: [name.atkrnKAMAZVEZDEHOD[2],name.atkrnMaz[2], name.atkrURAL55733[2], name.atkrKAMAZ65115[2], name.atkrKAMAZ63501[2]],
     },
-    mine: { 
+    mineM: { 
         img: [name.mineKubota[1], name.mineHitachiEX30[1], name.mineTakeuchiTB016[1]],
         title: [name.mineKubota[0], name.mineHitachiEX30[0], name.mineTakeuchiTB016[0]],
         description: ['amet consectetur adipisicing elit.'],
@@ -103,7 +102,7 @@ let all = {
         link: [name.mineKubota[2], name.mineHitachiEX30[2], name.mineTakeuchiTB016[2]],
         
     },
-    samosval: { 
+    samosvalM: { 
         img: [name.smsvlKamaz6520[1], name.smsvlKamaz65115[1] ,name.smsvlReanaultKerax[1]],
         title: [name.smsvlKamaz6520[0], name.smsvlKamaz65115[0] ,name.smsvlReanaultKerax[0]],
         description: ['amet consectetur adipisicing elit.'],
@@ -117,7 +116,7 @@ let all = {
         bodyVolume:['18', '10-12', '20'],
         link: [name.smsvlKamaz6520[2], name.smsvlKamaz65115[2] ,name.smsvlReanaultKerax[2]],
     },
-    ekskGusenich: { 
+    ekskGusenichM: { 
         img:  [name.egusHitachi330[1], name.egusHitachi300[1], name.egusVOLVOEC240[1], name.egusVOLVOEC240LOng[1]],
         title: [name.egusHitachi330[0], name.egusHitachi300[0], name.egusVOLVOEC240[0], name.egusVOLVOEC240LOng[0]],
         description: ['amet consectetur adipisicing elit.', 'amet consectetur adipisicing elit.','amet consectetur adipisicing elit.' ],
@@ -130,7 +129,7 @@ let all = {
         liftingHeight: ['7.63', '10.62', '12.579', '18.31' ],
         link:  [name.egusHitachi330[2], name.egusHitachi300[2], name.egusVOLVOEC240[2], name.egusVOLVOEC240LOng[2]],
     },
-    ekskKoles: { 
+    ekskKolesM: { 
         img: [name.ekolDoosan[1], name.ekolJCBJS200W[1], name.ekolVOLVOEW160[1]],
         title: [name.ekolDoosan[0], name.ekolJCBJS200W[0], name.ekolVOLVOEW160[0]],
         description: ['amet consectetur adipisicing elit.'],
@@ -144,7 +143,7 @@ let all = {
         liftingHeight:['9.026', '7.3', '9.48'] ,
         link: [name.ekolDoosan[2], name.ekolJCBJS200W[2], name.ekolVOLVOEW160[2]],
     },
-    ekskPogr: { 
+    ekskPogrM: { 
         img: [name.epogTLB825[1], name.epogJSB4CX[1], name.epogTEREX860[1]],
         title: [name.epogTLB825[0], name.epogJSB4CX[0], name.epogTEREX860[0]],
         description: ['amet consectetur adipisicing elit.'],
@@ -160,7 +159,7 @@ let all = {
         carryingСapacity: ['3.2', '4.64', '3.46',  ],   
         link: [name.epogTLB825[2], name.epogJSB4CX[2], name.epogTEREX860[2]],
     },
-    teleskPogr: { 
+    teleskPogrM: { 
         img: [name.teleskPogrBobcatT40170[1], name.teleskPogrFaresinFH1442[1], name.teleskPogrJCBAGRI[1]],
         title: [name.teleskPogrBobcatT40170[0], name.teleskPogrFaresinFH1442[0], name.teleskPogrJCBAGRI[0]],
         description: ['amet consectetur adipisicing elit.'],
@@ -172,7 +171,7 @@ let all = {
         carryingСapacity:['4', '4.2', '3.6'],
         link: [name.teleskPogrBobcatT40170[2], name.teleskPogrFaresinFH1442[2], name.teleskPogrJCBAGRI[2]],
     },
-    minPogruz: { 
+    minPogruzM: { 
         img: [name.minPogruzWECAN800D[1], name.minPogruzTAKEUCHI[1], name.minPogruzNEWHOLLANDL218[1]],
         title: [name.minPogruzWECAN800D[0], name.minPogruzTAKEUCHI[0], name.minPogruzNEWHOLLANDL218[0]],
         description: ['amet consectetur adipisicing elit.'],
@@ -185,7 +184,7 @@ let all = {
         carryingСapacity:['0.8', '2.7' , '0.818'],
         link: [name.minPogruzWECAN800D[2], name.minPogruzTAKEUCHI[2], name.minPogruzNEWHOLLANDL218[2]],
     },
-    frontPogruz: { 
+    frontPogruzM: { 
         img: [name.frontPogruzVOLVOL90F[1], name.frontPogruzLG936L[1], name.frontPogruzLG933L[1]],
         title: [name.frontPogruzVOLVOL90F[0], name.frontPogruzLG936L[0], name.frontPogruzLG933L[0]],
         description: ['amet consectetur adipisicing elit.'],
@@ -199,7 +198,7 @@ let all = {
         turnRadius:['5.92', '5.912', '5.912'],
         link: [name.frontPogruzVOLVOL90F[2], name.frontPogruzLG936L[2], name.frontPogruzLG933L[2]],
     },
-    mnpltr: { 
+    mnpltrM: { 
         img: [name.mnpltrHD78[1], name.mnpltrHD120[1], name.mnpltrKamaz[1]],
         title: [name.mnpltrHD78[0], name.mnpltrHD120[0], name.mnpltrKamaz[0]],
         description: ['amet consectetur adipisicing elit.'],
@@ -250,13 +249,13 @@ const getTechnics = (nameTechnics) => {
 }
 
 /*Создаем список техники по умолчанию на страницах "Главная" и "Техника" */
-    let defaultList = (selectedTechnic === null || selectedTechnic === 'allTechnics' ) ? all.autokran: all[selectedTechnic];
-    let choiceList = (selectedTechnic === null || selectedTechnic === 'allTechnics') ? 'autokran': selectedTechnic;
-    if ($('#allTechnics').length === 0) {  
+    let defaultList = (selectedTechnic === null || selectedTechnic === 'allTechnics' ) ? all.autokranM: all[selectedTechnic];
+    let choiceList = (selectedTechnic === null || selectedTechnic === 'allTechnics') ? 'autokranM': selectedTechnic;
+    if ($('#mainPageCatalog').length !==0) {  
             $(`#${choiceList}`).addClass("techs-active");
             getTechnics(defaultList);
     }
-    else if ($('#allTechnics').length !== 0) {
+    else if ($('#allTechnics').length !==0) {
             if (selectedTechnic === 'allTechnics' ) {
                 for (let key in all) {
                     let defaultList = all[key];
@@ -269,6 +268,53 @@ const getTechnics = (nameTechnics) => {
                 getTechnics(defaultList);
             }
     }
+    else if ($('#autokrans').length !==0 ) {
+        $('#autokrans').addClass("techs-active");
+        getTechnics(all.autokranM);
+    }
+    else if ($('#excavatory-gusenichnye').length !==0 ) {
+        $('#excavatory-gusenichnye').addClass("techs-active");
+        getTechnics(all.ekskGusenichM);
+    }
+    else if ($('#excavatory-kolesnye').length !==0 ) {
+        $('#excavatory-kolesnye').addClass("techs-active");
+        getTechnics(all.ekskKolesM);
+    }
+    else if ($('#excavatory-pogruzchiki').length !==0 ) {
+        $('#excavatory-pogruzchiki').addClass("techs-active");
+        getTechnics(all.ekskPogrM);
+    }
+
+    else if ($('#frontalnye-pogruzchiki').length !==0 ) {
+        $('#frontalnye-pogruzchiki').addClass("techs-active");
+        getTechnics(all.frontPogruzM);
+    }
+
+    else if ($('#mini-excavatory').length !==0 ) {
+        $('#mini-excavatory').addClass("techs-active");
+        getTechnics(all.mineM);
+    }
+
+    else if ($('#mini-pogruzchiki').length !==0 ) {
+        $('#mini-pogruzchiki').addClass("techs-active");
+        getTechnics(all.minPogruzM);
+    }
+
+    else if ($('#manipulators').length !==0 ) {
+        $('#manipulators').addClass("techs-active");
+        getTechnics(all.mnpltrM);
+    }
+
+    else if ($('#samosavls').length !==0 ) {
+        $('#samosavls').addClass("techs-active");
+        getTechnics(all.samosvalM);
+    }
+
+    else if ($('#teleskopicheskie-pogruzchiki').length !==0 ) {
+        $('#teleskopicheskie-pogruzchiki').addClass("techs-active");
+        getTechnics(all.teleskPogrM);
+    }
+
 
 /*Меняем список техники по клику*/
 $(".techs").click(function() {
@@ -321,7 +367,7 @@ if ($(".tech")) {
                     blockMenu = `<li class='btn technic_active'><a class='nav-link' href=${name[technic][2]}>${name[technic][0]}</a></li>`;
                 }
                 else {
-                    blockMenu = `<li class='btn btn-secondary '><a class='nav-link' href=${name[technic][2]}>${name[technic][0]}</a></li>`;
+                    blockMenu = `<li class='btn btn-light technic_passive '><a class='nav-link' href=${name[technic][2]}>${name[technic][0]}</a></li>`;
                 }
                 return blockMenu;
             }));
@@ -371,25 +417,3 @@ if ($(".tech")) {
         selectType = $(".card-body")
             .append(table);
     }
-
-    
-    
-
-
-    //Аккордеон
-
-// $('.panel-collapse [data-toggle="collapse"]').on('click', function() {
-// 	var $target = $(this).parents('.panel-collapse');
-// 	$target.removeClass('in').addClass('show');
-// 	setTimeout(function() {
-// 	  $target.removeClass('show').addClass('in');
-// 	}, 10)
-//   });
-   
-    
-
-
-
-
-
-    
